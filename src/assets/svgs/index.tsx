141024220702589abc3e59bb1d8e0ca3c13d383b
lazy-sky <1cielo@naver.com>
2022-05-28 15:03:05 +0900

    Asset: Upload icon images

---
+++ src/assets/svgs/index.tsx
@@ -0,0 +1,4 @@
+export { ReactComponent as OptionsIcon } from './options.svg'
+export { ReactComponent as LikePressedIcon } from './like-press.svg'
+export { ReactComponent as LikeUnpressedIcon } from './like-unpress.svg'
+export { ReactComponent as CommentIcon } from './comment.svg'
