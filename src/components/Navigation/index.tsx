c856da502e71a9253f1cb9a721d0a9c799f94db3
lazy-sky <1cielo@naver.com>
2022-05-30 21:50:32 +0900

    Design: Highlight active tab in Navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,29 +1,40 @@
 import { ChatIcon, HomeIcon, PencilIcon, UserIcon } from 'assets/svgs'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+import cx from 'classnames'
+import { isLoggedInState } from 'store/atom'
 
 import styles from './navigation.module.scss'
 
 const Navigation = () => {
+  const location = useLocation()
+  const { pathname } = location
+  const isLoggedIn = useRecoilValue(isLoggedInState)
+
+  const checkIsActive = (path: string) => {
+    return path === pathname
+  }
+
   return (
     <nav className={styles.navigation}>
       <ul>
         <li>
-          <Link to='/'>
+          <Link to='/' className={cx(checkIsActive('/') && styles.active)}>
             <HomeIcon />
           </Link>
         </li>
         <li>
-          <Link to='write'>
+          <Link to='write' className={cx(checkIsActive('/write') && styles.active)}>
             <PencilIcon />
           </Link>
         </li>
         <li>
-          <Link to='chat'>
+          <Link to='chat' className={cx(checkIsActive('/chat') && styles.active)}>
             <ChatIcon />
           </Link>
         </li>
         <li>
-          <Link to='profile'>
+          <Link to='profile' className={cx(checkIsActive('/profile') && styles.active)}>
             <UserIcon />
           </Link>
         </li>
