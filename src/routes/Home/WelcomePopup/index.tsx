90a115f860929e0d56a1e4e3e807adc302aaf7cb
lazy-sky <1cielo@naver.com>
2022-06-04 04:09:54 +0900

    Feat: Make WelcomePopup

---
+++ src/routes/Home/WelcomePopup/index.tsx
@@ -0,0 +1,73 @@
+import { MeerkatIcon } from 'assets/svgs'
+import { Dispatch, ReactNode, useMemo } from 'react'
+import { createPortal } from 'react-dom'
+
+import styles from './welcomePopup.module.scss'
+
+interface IPortal {
+  children: ReactNode
+  elementId: string
+}
+
+interface IWelcomePopup {
+  visible: boolean
+  setVisible: Dispatch<React.SetStateAction<boolean>>
+}
+
+// 나중에 모달, 팝업이 또 필요한 경우가 생기면 분리
+const Portal = ({ children, elementId }: IPortal) => {
+  const rootElement = useMemo(() => document.getElementById(elementId), [elementId])
+  return createPortal(children, rootElement!)
+}
+
+const WelcomePopup = ({ visible, setVisible }: IWelcomePopup) => {
+  const handleCloseClick = () => {
+    setVisible(false)
+  }
+  const handleNeverClick = () => {
+    localStorage.setItem('skyLoungeWelcome', 'no')
+  }
+
+  return (
+    <Portal elementId='popup'>
+      {visible && (
+        <div className={styles.popup}>
+          <div className={styles.wrapper}>
+            <h3>개발자들의 쉼터 Sky Lounge에 오신 것을 환영합니다!</h3>
+            <a href='https://github.com/lazy-sky' target='_blank' rel='noreferrer'>
+              <MeerkatIcon />
+            </a>
+            <p>Sky Lounge에서 할 수 있는 것!</p>
+            <ul>
+              <li>⭐프로젝트 자랑하기⭐</li>
+              <li>⭐스터디 모집⭐</li>
+              <li>⭐동료 모집⭐</li>
+              <li>⭐각종 홍보⭐</li>
+              <li>⭐모임 공지⭐</li>
+              <li>⭐익명 채팅⭐</li>
+              <li>⭐챌린지⭐</li>
+            </ul>
+            <div className={styles.footer}>
+              <div>
+                <a href='https://github.com/lazy-sky' target='_blank' rel='noreferrer'>
+                  @lazy-sky
+                </a>
+              </div>
+              <div>ⓒ 2022. @lazy-sky. all rights reserved.</div>
+            </div>
+          </div>
+          <div className={styles.closeBtns}>
+            <button type='button' onClick={handleNeverClick}>
+              다시 보지 않기
+            </button>
+            <button type='button' onClick={handleCloseClick}>
+              닫기
+            </button>
+          </div>
+        </div>
+      )}
+    </Portal>
+  )
+}
+
+export default WelcomePopup
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
+import { useMount } from 'react-use'
+
+import WelcomePopup from './WelcomePopup'
 import PageHeader from 'components/_shared/PageHeader'
 import PostsFeed from './PostsFeed'
 
 const Home = () => {
+  const [popUpVisible, setPopUpVisible] = useState(false)
+
+  useMount(() => {
+    const isVisited = localStorage.getItem('skyLoungeWelcome')
+    if (isVisited) return
+    setPopUpVisible(true)
+  })
+
   return (
     <>
+      {popUpVisible && <WelcomePopup visible={popUpVisible} setVisible={setPopUpVisible} />}
       <PageHeader title='Sky Lounge' />
       <PostsFeed />
     </>
