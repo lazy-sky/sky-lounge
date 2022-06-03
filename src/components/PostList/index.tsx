ccf2d9beb659eecda48495ae5348a0c359c13ecc
lazy-sky <1cielo@naver.com>
2022-06-03 19:45:39 +0900

    Comment: Memo about server issue

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,7 +1,6 @@
 import { IPost } from 'types/post'
 import PostItem from './PostItem'
 
-// TODO: 페이지네이션 및 무한 스크롤 적용
 const PostList = ({ posts }: { posts: IPost[] }) => {
   return (
     <ul>
--- src/routes/Home/PostsFeed/index.tsx
+++ src/routes/Home/PostsFeed/index.tsx
@@ -10,6 +10,8 @@ import styles from './postsFeed.module.scss'
 const PostsFeed = () => {
   const [posts, setPosts] = useState<IPost[]>([])
 
+  // TODO: 테스트 중 무료 플랜 한도 초과 발생
+  // 갑자기 유료 플랜으로 업그레이드하게 되어 무한 스크롤 및 레이지 로딩은 추후 구현
   useMount(() => {
     setPostsInRealTime(setPosts)
   })
