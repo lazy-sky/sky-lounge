03584e7c5cc31b4840ee030387b2c9f9bc4d9795
lazy-sky <1cielo@naver.com>
2022-06-05 01:00:17 +0900

    Comment: Memo about todo

--- src/routes/SignIn/index.tsx
+++ src/routes/SignIn/index.tsx
@@ -31,6 +31,10 @@ const SignIn = () => {
   }
 
   // TODO: iframe CORS 해결
+  // memo: 다른 origin을 가진 iframe DOM 트리에 접근하는 경우라 원칙적으로 에러 로그가 뜨는 게 맞다.
+  // 이 경우는 OAuth를 이용할 뿐 DOM을 조작하려는 것은 아니기 때문에 콘솔에 에러가 뜨는 것을 제외하면 따로 문제되는 것은 없다.
+  // 따라서 해결 후순위로 두자.
+  // popup 방식 대신 redirect로 로그인하게 하면 해결되지만 편의상 팝업이 더 좋다는 판단.
   const handleSocialLoginClick = async (event: MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
